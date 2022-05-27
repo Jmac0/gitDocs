730374f614eed5783a029619ffa63f8990655726
JamieMac <Jamiejmackenzie@gmail.com>
2022-05-27 20:58:03 +0100

    6

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const important = () => {
   const one = 1;
   const two = 2;
   const three = 3;
-  const feature = 4;
+  const four = 4;
   const five = 5;
-
+  const six = 6;
 }
 const A = true;
 const feature = 'feature';
