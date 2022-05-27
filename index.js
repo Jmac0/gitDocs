11b9554e9d73388a0d7df848c71e279a542c2f47
JamieMac <Jamiejmackenzie@gmail.com>
2022-05-27 19:54:05 +0100

    Bugfix on production

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function fea () {
   console.log('new stuff on new branch');
 
 }
+
+const bugfix = function(bug) {
+  return bug + 2
+}
 const newThingOnFeature = true;
 const newThing = 'new master'
 // only on jimbob
